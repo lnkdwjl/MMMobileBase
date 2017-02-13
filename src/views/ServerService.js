7d815017b2bf84ca00bf8edbbef83fa5035dcb82
wangjinlai <wangjinlai@sunmap.com>
2017-02-13 15:18:57 +0800

    基本函数修改

--- src/views/ServerService.js
+++ src/views/ServerService.js
@@ -3,21 +3,56 @@
  */
 define(function(require,exports,module) {
     var FlyJSONP = require("FlyJSONP");
-    var downUrl = seajs.config.data.service_urls.getDownUrl;
+    var $ = require("$");
     var ServerService = function(){};
     ServerService.prototype = {
-        request: function (url, data, success, error) {
+        /**
+         * @method requestJSONP
+         * @param url {String} 请求地址
+         * @param data {Object} 请求参数
+         * @param success {Function} 请求成功的回调方法
+         * @param [error] {Function} 请求失败的回调方法
+         */
+        requestJSONP: function (url, data, success, error) {
             FlyJSONP.get({
                 "url": url,
                 "parameters": data,
-                success: function () {
+                "success": function () {
                     typeof success === "function" && success.apply(this, arguments);
                 },
-                "error": error
+                "error" : function(msg) {
+                    typeof error === "function" && error.apply(this, arguments);
+                }
             })
         },
-        getDownUrl : function(data,success,error){
-            this.request(downUrl,data,success,error);
+        /**
+         * @method requestAjax
+         * @param url {String} 请求地址
+         * @param data {Object} 请求参数
+         * @param success {Function} 请求成功的回调方法
+         * @param [error] {Function} 请求失败的回调方法
+         * @param [options] {Object} 请求扩展参数
+         * @return {UserCenter} 当前对象
+         */
+        requestAjax : function(url, data, success, error, options) {
+            var request_params = {
+                "url" : url,
+                "type" : options.method,
+                "dataType" : options.dataType || "json",
+                "contentType" : options.contentType || "application/json",
+                "data" : data,
+                "cache" : false,
+                "headers" : {
+                    "Accept" : "application/vnd.uucin.v2+json"
+                },
+                "success" : function(msg) {
+                    typeof success === "function" && success.apply(this, arguments);
+                },
+                "error" : function(msg) {
+                    typeof error === "function" && error.apply(this, arguments);
+                }
+            };
+            $.ajax(request_params);
             return this;
         }
     }
--- src/views/router.js
+++ src/views/router.js
@@ -1,56 +1,18 @@
 define(function(require) {
     require("views/utils")
     seajs.config({
+        //根据config.js v配置设置不同的请求路径，常到开发环境正式环境设置
         service_urls : (function(v) {
             var service_urls = {};
             if (v === "R") {
-                service_urls.getDownUrl = "http://travel.mxnavi.com:8788/ln_bms_shop/manager/category/getDownUrl";
-                service_urls.localTourService = "http://service.mxlvniao.com/service/";
+                //service_urls.getDownUrl = "正式地址";
             } else if (v === "T") {
-                service_urls.getDownUrl = "http://travel.mxnavi.com:8788/ln_bms_shop/manager/category/getDownUrl";
-                service_urls.localTourService = "http://192.168.12.216:9007/service/";
+                //service_urls.getDownUrl = "测试地址";
             } else if (v === "D") {
-                service_urls.getDownUrl = "http://travel.mxnavi.com:8788/ln_bms_shop/manager/category/getDownUrl";
-                service_urls.localTourService = "http://192.168.12.216:9007/service/";
+                //service_urls.getDownUrl = "开发地址";
             }
             return service_urls;
         })(seajs.config.data.v)
     });
-    var _hmt = _hmt || [];
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "//hm.baidu.com/hm.js?f088c4809ea0d067009e4159a1c8fdcb";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-    })();
-    var Service = require('views/ServerService')
-    var service = new Service();
-    var data20500 = {productLine:'20500'};
-    var data20600 = {productLine:'20600'};
-    var data10500 = {productLine:'10500'};
-    var data10600 = {productLine:'10600'};
-    var error = function(){
-        console.log(arguments)
-    };
-    var success20500 = function(){
-        var url = arguments[0].downUrl;
-        seajs.config.data.service_urls.japanAndroidDown = url;
-    }
-    var success20600 = function(){
-        var url = arguments[0].downUrl;
-        seajs.config.data.service_urls.koreaAndroidDown = url;
-    }
-    var success10500 = function(){
-        var url = arguments[0].downUrl;
-        seajs.config.data.service_urls.japanIosDown = url;
-    }
-    var success10600 = function(){
-        var url = arguments[0].downUrl;
-        seajs.config.data.service_urls.koreaIosDown = url;
-    }
-    service.getDownUrl(data20500,success20500,error);
-    service.getDownUrl(data20600,success20600,error);
-    service.getDownUrl(data10500,success10500,error);
-    service.getDownUrl(data10600,success10600,error);
     require.async("views/application");
 });
