6c604994edcd741e05d0a4af4d452c83ec76aeb6
wangjinlai <wangjinlai@sunmap.com>
2017-02-15 13:24:33 +0800

    更新

--- src/views/footer.js
+++ src/views/footer.js
@@ -4,15 +4,48 @@
 define(function(require) {
     var ko = require("ko");
     var View = require('View');
+    var $ = require("$");
     var view_footer = new View({
         id : "view_footer",
         html : require("/src/templates/footer.html"),
         afterRender : function() {
             view_footer.bindViewModel(viewModel);
+            viewModel.currentDom = $(".homeSelect")[0];
         }
     });
     var viewModel = view_footer.viewModel = {
-
+        currentDom:null,
+        homeClick:function(data,event){
+            event.stopPropagation();
+            viewModel.addClass(event);
+        },
+        alarmClick:function(data,event){
+            event.stopPropagation();
+            viewModel.addClass(event);
+        },
+        orderClick:function(data,event){
+            event.stopPropagation();
+            viewModel.addClass(event);
+        },
+        userClick:function(data,event){
+            event.stopPropagation();
+            viewModel.addClass(event);
+        },
+        refreshAllClass:function(dom){
+            if(viewModel.currentDom!==dom){
+                var className = $(viewModel.currentDom).attr("class").replace("Select","");
+                $(viewModel.currentDom).removeClass();
+                $(viewModel.currentDom).addClass(className);
+            }
+        },
+        addClass:function(event){
+            var dom = event.currentTarget;
+            viewModel.refreshAllClass(dom);
+            var className = $(dom).attr("class")+"Select";
+            $(dom).removeClass();
+            $(dom).addClass(className);
+            viewModel.currentDom = dom;
+        }
     }
 
     return view_footer;
